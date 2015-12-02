ec4fad273edd81035033f95ae23bc19feb1d8328
Anna Sherman <sherman.anna@gmail.com>
2015-12-02 11:32:32 -0600

    improved database

--- db/seed.sql
+++ db/seed.sql
@@ -1,12 +1,24 @@
 # insert a few rows into your table
 
-INSERT INTO tasks (name, description) VALUES ('Norma', 'Mozilla/5.0 (Windows; U; Windows NT 6.1; tr-TR) AppleWebKit/533.20.25 (KHTML, like Gecko) Version/5.0.4 Safari/533.20.27');
-INSERT INTO tasks (name, description) VALUES ('Christopher', 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_3; en-au) AppleWebKit/533.16 (KHTML, like Gecko) Version/5.0 Safari/533.16');
-INSERT INTO tasks (name, description) VALUES ('Linda', 'Mozilla/5.0 (Macintosh; U; PPC Mac OS X 10_4_11; nl-nl) AppleWebKit/533.16 (KHTML, like Gecko) Version/4.1 Safari/533.16');
-INSERT INTO tasks (name, description) VALUES ('Ernest', 'Mozilla/5.0 (Windows NT 6.0; WOW64) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.66 Safari/535.11');
-INSERT INTO tasks (name, description) VALUES ('Deborah', 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_7; da-dk) AppleWebKit/533.21.1 (KHTML, like Gecko) Version/5.0.5 Safari/533.21.1');
-INSERT INTO tasks (name, description) VALUES ('Martin', 'Mozilla/5.0 (Windows NT 6.1; rv:27.3) Gecko/20130101 Firefox/27.3');
-INSERT INTO tasks (name, description) VALUES ('Carolyn', 'Mozilla/5.0 (Windows; U; Windows NT 6.1; WOW64; en-US; rv:2.0.4) Gecko/20120718 AskTbAVR-IDW/3.12.5.17700 Firefox/14.0.1');
-INSERT INTO tasks (name, description) VALUES ('Beverly', 'Mozilla/5.0 (Windows NT 6.0) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/13.0.782.20 Safari/535.1');
-INSERT INTO tasks (name, description) VALUES ('Wayne', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_3) AppleWebKit/534.55.3 (KHTML, like Gecko) Version/5.1.3 Safari/534.53.10');
-INSERT INTO tasks (name, description) VALUES ('Emily', 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_6; ko-kr) AppleWebKit/533.20.25 (KHTML, like Gecko) Version/5.0.4 Safari/533.20.27');
+-- INSERT INTO tasks (name, description) VALUES ('Norma', 'Mozilla/5.0 (Windows; U; Windows NT 6.1; tr-TR) AppleWebKit/533.20.25 (KHTML, like Gecko) Version/5.0.4 Safari/533.20.27');
+-- INSERT INTO tasks (name, description) VALUES ('Christopher', 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_3; en-au) AppleWebKit/533.16 (KHTML, like Gecko) Version/5.0 Safari/533.16');
+-- INSERT INTO tasks (name, description) VALUES ('Linda', 'Mozilla/5.0 (Macintosh; U; PPC Mac OS X 10_4_11; nl-nl) AppleWebKit/533.16 (KHTML, like Gecko) Version/4.1 Safari/533.16');
+-- INSERT INTO tasks (name, description) VALUES ('Ernest', 'Mozilla/5.0 (Windows NT 6.0; WOW64) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.66 Safari/535.11');
+-- INSERT INTO tasks (name, description) VALUES ('Deborah', 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_7; da-dk) AppleWebKit/533.21.1 (KHTML, like Gecko) Version/5.0.5 Safari/533.21.1');
+-- INSERT INTO tasks (name, description) VALUES ('Martin', 'Mozilla/5.0 (Windows NT 6.1; rv:27.3) Gecko/20130101 Firefox/27.3');
+-- INSERT INTO tasks (name, description) VALUES ('Carolyn', 'Mozilla/5.0 (Windows; U; Windows NT 6.1; WOW64; en-US; rv:2.0.4) Gecko/20120718 AskTbAVR-IDW/3.12.5.17700 Firefox/14.0.1');
+-- INSERT INTO tasks (name, description) VALUES ('Beverly', 'Mozilla/5.0 (Windows NT 6.0) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/13.0.782.20 Safari/535.1');
+-- INSERT INTO tasks (name, description) VALUES ('Wayne', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_3) AppleWebKit/534.55.3 (KHTML, like Gecko) Version/5.1.3 Safari/534.53.10');
+-- INSERT INTO tasks (name, description) VALUES ('Emily', 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_6; ko-kr) AppleWebKit/533.20.25 (KHTML, like Gecko) Version/5.0.4 Safari/533.20.27');
+
+
+INSERT INTO tasks (name, description) VALUES ('Pink', 'In eleifend quam a odio.');
+INSERT INTO tasks (name, description) VALUES ('Blue', 'Pellentesque eget nunc.');
+INSERT INTO tasks (name, description) VALUES ('Maroon', 'Etiam justo.');
+INSERT INTO tasks (name, description) VALUES ('Fuscia', 'Maecenas leo odio, condimentum id, luctus nec, molestie sed, justo.');
+INSERT INTO tasks (name, description) VALUES ('Turquoise', 'Vestibulum sed magna at nunc commodo placerat.');
+INSERT INTO tasks (name, description) VALUES ('Aquamarine', 'Suspendisse potenti. Nullam porttitor lacus at turpis.');
+INSERT INTO tasks (name, description) VALUES ('Red', 'Nam tristique tortor eu pede.');
+INSERT INTO tasks (name, description) VALUES ('Indigo', 'Aliquam non mauris. Morbi non lectus.');
+INSERT INTO tasks (name, description) VALUES ('Orange', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla.');
+INSERT INTO tasks (name, description) VALUES ('Yellow', 'Aliquam sit amet diam in magna bibendum imperdiet.');
